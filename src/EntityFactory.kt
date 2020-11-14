8a8dfbab7a69e4bc21bfec0877dfdac6825ea9f4
ridmik <poran.cse@gmail.com>
2020-11-15 00:39:12 +0600

    implement Enum class

--- src/EntityFactory.kt
+++ src/EntityFactory.kt
@@ -1,22 +1,29 @@
+import java.util.*
 
 interface IdProvider{
     fun getId():String
 }
 
-object  EntityFactory{
-    fun create()=Entity("id1202")
+enum class EntityType{
+    Easy,Medium,Hard;
+    fun formattedName()=name.capitalize()
+    fun toLow()=name.toLowerCase()
+    fun toUp()=name.toUpperCase()
+
 }
-class  Entity (val id:String){
-    companion object:IdProvider{
-        private const val id="Id"
-        fun create(){
-            this.getId()
-            Entity(id = getId())}
-        override fun getId() :String{
-            println("Your id $id")
-            return "Your id $id"
+
+object  EntityFactory{
+    fun create(type:EntityType):Entity{
+        val id=UUID.randomUUID().toString()
+        val  name=when(type){
+            EntityType.Easy-> type.formattedName()
+            EntityType.Medium-> type.toLow()
+            EntityType.Hard->type.toUp()
         }
+        return  Entity(id,name)
     }
+}
+class  Entity (private val id:String, private val name:String){
 
     fun created(){
         println("Created factory")
@@ -24,16 +31,20 @@ class  Entity (val id:String){
     }
 
     override fun toString(): String {
-        return "Your id is $id"
+        return "Your id is $id  and type $name"
     }
 
 }
 
 fun main() {
-    var entity=Entity.create()
-    var entityFact=EntityFactory.create()
-  //  entityFact.created()
-    println(entityFact)
+    var easy=EntityFactory.create(EntityType.Easy)
+    var medium=EntityFactory.create(EntityType.Medium)
+    var hard=EntityFactory.create(EntityType.Hard)
+
+    //  entityFact.created()
+    println(easy)
+    println(medium)
+    println(hard)
 
 
 }

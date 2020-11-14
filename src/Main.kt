fc61e6098dcc656cb1f8b3b2378985e594b4e296
ridmik <poran.cse@gmail.com>
2020-11-14 13:50:44 +0600

    added mutable map

--- src/Main.kt
+++ src/Main.kt
@@ -5,4 +5,18 @@
 
          map.forEach { (k, v) -> println("$k -> $v")}
 
+         //map.put(4)="four" will show compile error cause mapOf() immutable
+
+         val mutableMap= mutableMapOf("one" to 1,"two" to 2, "three" to 3, "four" to 4)
+         mutableMap.forEach { (k, v) -> println("$k -> $v")}
+
+         print("after add new value\n")
+         mutableMap["five"] = 5
+         mutableMap.forEach { (k, v) -> println("$k -> $v")}
+
+
+
+
+
+
      }
